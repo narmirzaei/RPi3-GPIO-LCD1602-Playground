fff4813efcfdc5af8390c4101322d66ee64c64bf
Narbeh Mirzaei <narmirzaei@gmail.com>
2017-10-05 16:39:45 +0330

    Hello world with blinking LED

--- hello_world.py
+++ hello_world.py
@@ -1,16 +1,20 @@
 #!/usr/bin/python
 import time
 import atexit
+# https://github.com/adafruit/Adafruit_Python_CharLCD/blob/master/Adafruit_CharLCD/Adafruit_CharLCD.py
 import Adafruit_CharLCD as LCD
 
 # Initialize the LCD using the pins
 lcd = LCD.Adafruit_CharLCDPlate()
 
-print 'Registering atexit.stop'
-atexit.register(stop)
-print 'Registered atexit.stop'
-
-lcd.set_color(0.0, 0.0, 1.0)
 lcd.clear()
 lcd.message('Hello, World!')
-time.sleep(3.0)
+
+print('Press Ctrl-C to quit.')
+while True:
+	lcd.set_color(1.0, 0.0, 0.0)
+	time.sleep(1.0)
+	lcd.set_color(0.0, 1.0, 0.0)
+	time.sleep(1.0)
+	lcd.set_color(0.0, 0.0, 1.0)
+	time.sleep(1.0)
