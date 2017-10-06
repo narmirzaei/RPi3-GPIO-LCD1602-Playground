ed3ec677c36c6b0c50be7f3cb43dea57ed7da185
Narbeh Mirzaei <narmirzaei@gmail.com>
2017-10-06 19:02:54 +0330

    Check Heroku apps heartbeat

---
+++ index.js
@@ -0,0 +1,40 @@
+const schedule = require('node-schedule');
+const http = require('http');
+const LCDPLATE = require('adafruit-i2c-lcd').plate;
+var lcd = new LCDPLATE(1, 0x20);
+
+lcd.backlight(lcd.colors.WHITE);
+lcd.clear();
+
+var urls = [
+  'https://aurore-t-001.herokuapp.com/',
+  'https://aurore-t-002.herokuapp.com/',
+  'https://aurore-t-003.herokuapp.com/',
+  'https://aurore-t-004.herokuapp.com/',
+  'https://aurore-t-005.herokuapp.com/'
+]
+
+var job = schedule.scheduleJob('*/1 * * * *', function() {
+  var total = 0, count = 0
+  urls.forEach(function(value) {
+    lcd.backlight(lcd.colors.OFF);
+    count += 1
+    http.get(url, function(res) {
+      if(res.statusCode == 200) {
+        total += 1
+      }
+      lcd.clear();
+      lcd.message(`${count} / ${urls.length}`);
+      lcd.backlight(lcd.colors.ON);
+    });
+  });
+
+  lcd.clear();
+  if(total == urls.length) {
+    lcd.message("All Good!");
+    lcd.backlight(lcd.colors.GREEN);
+  } else {
+    lcd.message(`${urls.length - total} Down`);
+    lcd.backlight(lcd.colors.RED);
+  }
+});
