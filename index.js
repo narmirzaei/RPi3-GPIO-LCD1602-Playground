b9110fd31407366f2ba997703df722d73ebbe47a
Narbeh Mirzaei <narmirzaei@gmail.com>
2017-10-07 01:04:11 +0330

    Using new Https get method

--- index.js
+++ index.js
@@ -8,21 +8,40 @@ lcd.sendBytes(0, 0x1F); // Sainsmart 1602 I2C backlight on
 lcd.backlight(lcd.colors.WHITE);
 lcd.clear();
 
-var urls = [
-  'https://aurore-t-001.herokuapp.com/',
-  'https://aurore-t-002.herokuapp.com/',
-  'https://aurore-t-003.herokuapp.com/',
-  'https://aurore-t-004.herokuapp.com/',
-  'https://aurore-t-005.herokuapp.com/'
-]
+endpoints =
+ [{ host: 'https://aurore-t-001.herokuapp.com', path: '/' },
+  { host: 'https://aurore-t-002.herokuapp.com', path: '/' },
+  { host: 'https://aurore-t-003.herokuapp.com', path: '/' },
+  { host: 'https://aurore-t-004.herokuapp.com', path: '/' },
+  { host: 'https://aurore-t-005.herokuapp.com', path: '/' }];
+
+var total, count
 
 var job = schedule.scheduleJob('*/1 * * * *', function() {
   console.log("Job started running ...")
 
+<<<<<<< Updated upstream
+  total = 0, count = 0
+=======
+  async.mapSeries(endpoints, https.get, function(results){
+      results.forEach(function(result) {
+        if(result.statusCode == 200) {
+          console.log(`${url} is UP`)
+        } else {
+          console.log(`${url} is DOWN`)
+        }
+      })
+  });
+});
+
+/*
   var total = 0, count = 0
+>>>>>>> Stashed changes
   urls.forEach(function(url) {
     count += 1
     console.log(`Processing url ${url}`)
+    
+    sleep.sleep(1) 
     https.get(url, function(res) {
       if(res.statusCode == 200) {
         total += 1
@@ -34,9 +53,9 @@ var job = schedule.scheduleJob('*/1 * * * *', function() {
       lcd.clear();
       lcd.message(`${count} / ${urls.length}`);
 
-      sleep.sleep(2) //Sleep 1 second
+      sleep.sleep(1) //Sleep 1 second
 
-      if(count == urls.legnth) {
+      if(count >= urls.legnth) {
         lcd.clear();
         if(total == urls.length) {
           console.log("All urls are UP")
@@ -50,7 +69,7 @@ var job = schedule.scheduleJob('*/1 * * * *', function() {
         console.log("Job finished running.")
       }
     });
-  });
-});
+  });*/
+
 
 console.log("Job is scheduled to run every 1 minute...")
